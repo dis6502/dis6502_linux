d4a255870c78cb832b91944efed0fc293aea7515
Peter Dell <peter.dell@web.de>
2026-08-27 00:41:20 +0200

    Update FileIO.cpp

--- src/FileIO.cpp
+++ src/FileIO.cpp
@@ -84,7 +84,12 @@ std::vector<wstring> FileIO::ReadStrings(wstring_view filePath) {
 }
 
 FILE* FileIO::OpenFile(wstring_view filePath, wstring_view mode) {
+#ifdef _WIN32
     FILE* fd = _wfopen(wstring(filePath).c_str(), wstring(mode).c_str());
+#else
+	auto path=FileIO::ToPath(filePath);
+    FILE* fd = fopen(path.c_str(), String::wstring_to_utf8(mode).c_str());
+#endif
 
     if (fd == 0) {
         errno_t errorNumber;
